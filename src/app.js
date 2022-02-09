9757f4fa87d0033654376f5586ac089bc2905541
Saurabh <upadhyaysaurabh5960@gmail.com>
2022-02-10 02:35:55 +0530

    Created Main port

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const hbs = require("hbs");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const base = path.join(__dirname, "../public");
 const vp = path.join(__dirname, "../templates/views");
